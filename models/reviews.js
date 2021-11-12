6b18a97a0b7d57b2ddf4c4719136dda2e35c4760
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-11-11 20:40:17 -0500

    updated seeds to reflect new models

--- models/reviews.js
+++ models/reviews.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, Sequelize } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Review extends Model { }
@@ -45,6 +45,7 @@ Review.init(
         review_date: {
             type: DataTypes.DATE,
             allowNull: false,
+            defaultValue: Sequelize.NOW
         },
         user_id: {
             type: DataTypes.INTEGER,
