8795978fab467946ee17de7ee7086799bdd1b55b
Zmoore <zmoore371@gmail.com>
2021-11-14 18:28:19 -0500

    Able to create review

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -54,24 +54,46 @@ const submitReviewHandler = async (event) => {
     const businessAddress = document.querySelector('#place-address').value.trim();
     const review = document.querySelector('#review-freeform').value.trim();
     const recommend = document.querySelector('input[type=radio][name="recommend"]:checked').value
+    let city_id = 1
 
     console.log(title, city, rating, category, business, businessAddress, recommend, review)
-    data = {title, city, rating, category, business, businessAddress, recommend, review}
+    data = {title, city, rating, category, business, businessAddress, recommend, review, city_id}
     getCityId(data)
 }
 
-const getCityId = async (data) => {
-    console.log(data.city)
-    console.log(data)
-
-    let requestUrl = '/api/review/cityid/' + data.city
-    await fetch(requestUrl)
-            .then(response => response.json())
-            .then(city => console.log(city))
+const getCityId = async (reviewInfo) => {
     
+    let requestUrl = '/api/review/cityid/' + data.city
 
+    fetch(requestUrl)
+    .then(function(response) {
+        if (response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject(response)
+        }
+    })
+    .then(function (data) {
+        cityInfo = data
+        reviewInfo.city_id = cityInfo[0].id
+
+        postReview(reviewInfo)
+    })
 }    
 
+const postReview = async(data) => {
+    const response = await fetch('/api/review', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    })
+    if(response.ok) {
+        document.location.reload()
+    } else {
+        console.log(JSON.stringify(reviewInfo))
+        alert("Error")
+    }
+}
 
 document.querySelector('#review')
 document.addEventListener('submit', submitReviewHandler)
