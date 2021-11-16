8d95462a92ffeeb3aaa8f5ef2dbfafe0d9b83f96
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-11-16 14:06:58 -0500

    push to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  // secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
